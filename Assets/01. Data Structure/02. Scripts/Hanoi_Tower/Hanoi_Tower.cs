4b5ad5deeadb85e1f2967b7841af008a5aa853ee
ZeroDimen <rlaehdrjs951@naver.com>
2025-07-11 17:47:04 +0900

    Add
    2025.07.11
    -- 탐색 알고리즘 간단 구현
    (선형탐색, 이진탐색, 이진 탐색 트리, 깊이 우선 탐색, 너비 우선 탐색)

--- Assets/01. Data Structure/02. Scripts/Hanoi_Tower/Hanoi_Tower.cs
+++ Assets/01. Data Structure/02. Scripts/Hanoi_Tower/Hanoi_Tower.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hanoi_Tower : MonoBehaviour
 {
@@ -16,11 +18,18 @@ public class Hanoi_Tower : MonoBehaviour
     public Board_Bar[] bars; // Left, Center, Right 순서
     
     public TextMeshProUGUI countTextUI;
+    public Button answerButton;
     
     public static GameObject selectedDonut;
     public static bool isSelected;
     public static Board_Bar currentBar;
     public static int moveCount;
+
+    private void Awake()
+    {
+        answerButton.onClick.AddListener(HanoiAnswer);
+    }
+
     private IEnumerator Start()
     {
         for (int i = (int)hanoiLevel; i > 0; i--) // 반복문으로 Level 만큼 도넛 생성
@@ -46,5 +55,28 @@ public class Hanoi_Tower : MonoBehaviour
             selectedDonut = null;
         }
         countTextUI.text = moveCount.ToString();
+    }
+
+    private void HanoiAnswer()
+    {
+        HanoiRutine((int)hanoiLevel,0,1,2);
+    }
+    private void HanoiRutine(int n, int from, int temp, int to)
+    {
+        if (n == 0) // 도넛을 다 옮긴 상태
+        {
+            return;
+        }
+
+        if (n == 1)
+        {
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+        }
+        else
+        {
+            HanoiRutine(n - 1, from, to, temp);
+            Debug.Log($"{n}번 도넛을 {from}에서 {to}로 이동");
+            HanoiRutine(n - 1, temp, from, to);
+        }
     }
 }
---
+++ Assets/02. Algorithm/02. Scripts/BinarySearchTree.cs
@@ -0,0 +1,100 @@
+using System;
+using UnityEngine;
+
+// 이진 탐색 트리의 예시 
+public class BinarySearchTree : MonoBehaviour 
+{
+    // 중첩 클레스
+    public class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+        public int value;
+
+        public TreeNode(int value)
+        {
+            this.value = value;
+        }
+    }
+
+    private TreeNode root;
+    private int[] array = { 8, 3, 10, 1, 6, 14, 4, 7, 13 }; // 배열
+    
+    private string result;
+
+    void Start()
+    {
+        foreach (var v in array)
+        {
+            root = Insert(root, v);
+        }
+        
+        PreOrder(root);
+        Debug.Log($"PreOrder : {result}");
+
+        result = String.Empty;
+        InOrder(root);
+        Debug.Log($"InOrder : {result}");
+        
+        result = String.Empty;
+        PostOrder(root);
+        Debug.Log($"PostOrder : {result}");
+    }
+    
+    private TreeNode Insert(TreeNode node, int value)
+    {
+        if (node == null)
+        {
+            return new TreeNode(value);
+        }
+
+        if (value < node.value)
+        {
+            node.left = Insert(node.left, value);
+        }
+        else
+        {
+            node.right = Insert(node.right, value);
+        }
+        return node;
+    }
+
+    private void PreOrder(TreeNode node) // 전위 순회
+    {
+        if (node == null)
+        {
+            return;
+        }
+        
+        result += $"{node.value}, ";
+        PreOrder(node.left);
+        PreOrder(node.right);
+    }
+
+    private void InOrder(TreeNode node) // 중위 순회
+    {
+        if (node == null)
+        {
+            return;
+        }
+           
+        InOrder(node.left);
+        result += $"{node.value}, ";
+        InOrder(node.right);
+    }
+
+    private void PostOrder(TreeNode node) // 후위 순회
+    {
+        if (node == null)
+        {
+            return;
+        }
+            
+        PostOrder(node.left);
+        PostOrder(node.right);
+        result += $"{node.value}, ";
+    }
+    
+    
+    
+}
